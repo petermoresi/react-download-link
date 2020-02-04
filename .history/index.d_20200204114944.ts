ee114fd5ed0e1ed0383bfb6e7bf496972db823de
Jamil Mammri <j.mammri@logenios.com>
2020-02-04 11:50:19 +0100

    added tagName to DownloadLinkProps

--- .history/index.d_20200204114944.ts
+++ .history/index.d_20200204114944.ts
@@ -4,10 +4,9 @@ interface DownloadLinkProps {
   filename?: string;
   label?: string | number | JSX.Element;
   style?: CSSProperties;
-  tagName?: string;
   exportFile?(type?: string): void;
 }
 
-declare class DownloadLink extends Component<DownloadLinkProps> { }
+declare class DownloadLink extends Component<DownloadLinkProps> {}
 
 export default DownloadLink;
