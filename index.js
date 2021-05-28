4ec3c9878fa313e93418559c4f89516f2c261094
Hernan Carrizo <owentar@gmail.com>
2021-05-28 09:42:09 -0300

    update index.js file

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports["default"] = void 0;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _react = _interopRequireDefault(require("react"));
 
-var _react = require("react");
+var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _react2 = _interopRequireDefault(_react);
-
-var _propTypes = require("prop-types");
-
-var _propTypes2 = _interopRequireDefault(_propTypes);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var DownloadLink = function (_Component) {
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var DownloadLink = /*#__PURE__*/function (_Component) {
   _inherits(DownloadLink, _Component);
+
+  var _super = _createSuper(DownloadLink);
 
   function DownloadLink() {
     _classCallCheck(this, DownloadLink);
 
-    return _possibleConstructorReturn(this, (DownloadLink.__proto__ || Object.getPrototypeOf(DownloadLink)).apply(this, arguments));
+    return _super.apply(this, arguments);
   }
 
   _createClass(DownloadLink, [{
     key: "handleDownloadClick",
     value: function handleDownloadClick(event) {
-      var _this2 = this;
+      var _this = this;
 
       event.persist();
       event.preventDefault();
@@ -42,17 +55,14 @@ var DownloadLink = function (_Component) {
       function magicDownload(text, fileName) {
         var blob = new Blob([text], {
           type: "text/csv;charset=utf8;"
-        });
+        }); // create hidden link
 
-        // create hidden link
         var element = document.createElement("a");
         document.body.appendChild(element);
         element.setAttribute("href", window.URL.createObjectURL(blob));
         element.setAttribute("download", fileName);
         element.style.display = "";
-
         element.click();
-
         document.body.removeChild(element);
         event.stopPropagation();
       }
@@ -62,7 +72,7 @@ var DownloadLink = function (_Component) {
 
       if (text instanceof Promise) {
         text.then(function (result) {
-          return magicDownload(result, _this2.props.filename);
+          return magicDownload(result, _this.props.filename);
         });
       } else {
         magicDownload(text, this.props.filename);
@@ -71,7 +81,7 @@ var DownloadLink = function (_Component) {
   }, {
     key: "render",
     value: function render() {
-      return _react2.default.createElement(this.props.tagName || "a", {
+      return /*#__PURE__*/_react["default"].createElement(this.props.tagName || "a", {
         role: this.props.role || undefined,
         "data-testid": this.props.testId || undefined,
         style: this.props.style,
@@ -96,12 +106,11 @@ DownloadLink.defaultProps = {
   },
   exportFile: function exportFile() {}
 };
-
 DownloadLink.propTypes = {
-  filename: _propTypes2.default.string,
-  label: _propTypes2.default.oneOfType([_propTypes2.default.string, _propTypes2.default.number, _propTypes2.default.object]),
-  style: _propTypes2.default.object,
-  exportFile: _propTypes2.default.func
+  filename: _propTypes["default"].string,
+  label: _propTypes["default"].oneOfType([_propTypes["default"].string, _propTypes["default"].number, _propTypes["default"].object]),
+  style: _propTypes["default"].object,
+  exportFile: _propTypes["default"].func
 };
-
-exports.default = DownloadLink;
+var _default = DownloadLink;
+exports["default"] = _default;
